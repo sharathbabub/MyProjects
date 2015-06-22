59dac75819899b6e03ae25e864965ba422f45083
sharathbabub <a7a.sharath@gmail.com>
2015-06-22 01:48:06 -0700

    Revert "List of My Projects"
    This reverts commit 7ed5f3c47ab85cd2de4a106ba957af88549d6b34.

--- Collaborative Recommendation Algorithm/text.py
+++
@@ -1,7 +0,0 @@
-mystr="Python is a great language\nlol\nhaha"
-print mystr
-fo = open("foo.txt", "w")
-fo.write( mystr)
-fo.write("xxxxxxxxx")
-
-fo.close()
--- SPJUAQueryEvaluator/queries/tpch6.sql
+++
@@ -1,9 +0,0 @@
-select
-sum(extendedprice*discount) as revenue
-from
-lineitem
-where
-shipdate >= DATE('1994-01-01')
-and shipdate < date ('1995-01-01')
-and discount >0.06 - 0.01 and discount<0.06+ 0.01 and quantity < 24;
-
